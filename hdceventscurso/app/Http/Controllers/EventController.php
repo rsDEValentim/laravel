b5a08a7e5b795f357cc2ccd3ea099ff1ee87b073
rsdevalentim@gmail.com <rsdriver1000@gmail.com>
2021-08-24 15:55:31 -0300

    avançando nas aulas

--- hdceventscurso/app/Http/Controllers/EventController.php
+++ hdceventscurso/app/Http/Controllers/EventController.php
@@ -13,12 +13,27 @@ class EventController extends Controller
 
         $events = Event::all();
     
-        return view('welcome', ['events' => $events]);
+        return view('welcome',['events' => $events]);
 
     }
 
     public function create() {
         return view('events.create');
+    }
+
+    public function store(Request $request) {
+
+        $event = new Event;
+
+        $event->title = $request->title;
+        $event->city = $request->city;
+        $event->private = $request->private;
+        $event->description = $request->description;
+
+        $event->save();
+
+        return redirect('/');
+
     }
 
 }
